15aca94513e8eb52a4d57fd8f9e071670516e0f1
Jason Chan <jasonchanhw@gmail.com>
2016-10-13 08:59:01 -0400

    Added command to generate private key in pkcs8 format.

--- gen-cert.sh
+++ gen-cert.sh
@@ -1,2 +1,3 @@
 openssl req -subj '/CN=log.example.org/' -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout fqdn-log.example.org.key -out fqdn-log.example.org.crt
+openssl pkcs8 -topk8 -inform PEM -outform DER -in fqdn-log.example.org.key -out fqdn-log.example.org.pkcs8 -nocrypt
 
